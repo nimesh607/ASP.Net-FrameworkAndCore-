0d072df17cc58722a960b50a3af979559e26e982
nimesh607 <nuvaishnav607@gmail.com>
2025-11-09 12:12:18 +0530

    Added .gitignore and cleaned unnecessary files

---
+++ FirstWebForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstWebForm.Global" Language="C#" %>
---
+++ FirstWebForm/Second.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebForm
+{
+    public partial class Second : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //In C# file write html ("")
+            Response.Write("Welcome to Second Page");
+            Response.Write("<br/>");
+            Response.Write("Server Date:" + DateTime.Now.ToShortDateString());
+            Response.Write("<br/>");
+            Response.Write("Server Time:" + DateTime.Now.ToShortTimeString());
+        }
+    }
+}
